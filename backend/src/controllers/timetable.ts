d81eb1332269ee1ccd14358e185b4ab2a308c831
Kristian <49764796+kristianka@users.noreply.github.com>
2024-03-18 12:19:01 +0200

    move requests to other file, add basic validation middleware

--- backend/src/controllers/timetable.ts
+++ backend/src/controllers/timetable.ts
@@ -1,74 +1,31 @@
 import express from "express";
-import axios from "axios";
-import { ClassResponse, CourseResponse } from "../../types";
+import { getByClass, getByCourse } from "./timetableRequests";
+import { validateCode } from "../middlewares";
 
 const timetableRouter = express.Router();
 
 // need to use query because get reqs don't have a body
-
+// class codes are validated by middleware validateCode
 // to do: ratelimit requests
 
 // by course code, like "5G00EV17-3003"
-timetableRouter.get("/course/", async (req, res, next) => {
+timetableRouter.get("/course/", validateCode, async (req, res, next) => {
   try {
-    // authenticate and authorize user before
-    const { code } = req.query;
-
-    if (!code || typeof code !== "string") {
-      return res
-        .status(400)
-        .send("Course code is required and must be a string.");
-    }
-
-    const apiRes = await axios.post<CourseResponse>(
-      "https://opendata.tamk.fi/r1/reservation/search",
-      {
-        realization: [code]
-      },
-      {
-        auth: {
-          username: process.env.TAMK_API_KEY || "",
-          password: ""
-        }
-      }
-    );
-
+    // todo: authenticate and authorize user before
+    const apiRes = await getByCourse(req.query.code as string);
     return res.send(apiRes.data);
   } catch (error) {
-    res.status(500).send("Internal server error. Please try again later.");
     return next(error);
   }
 });
 
 // by class code, like "21i224"
-timetableRouter.get("/class/", async (req, res, next) => {
+timetableRouter.get("/class/", validateCode, async (req, res, next) => {
   try {
-    // authenticate and authorize user before
-
-    const { code } = req.query;
-
-    if (!code || typeof code !== "string") {
-      return res
-        .status(400)
-        .send("Course code is required and must be a string.");
-    }
-
-    const apiRes = await axios.post<ClassResponse>(
-      "https://opendata.tamk.fi/r1/reservation/search",
-      {
-        studentGroup: [code]
-      },
-      {
-        auth: {
-          username: process.env.TAMK_API_KEY || "",
-          password: ""
-        }
-      }
-    );
-
+    // todo: authenticate and authorize user before
+    const apiRes = await getByClass(req.query.code as string);
     return res.send(apiRes.data);
   } catch (error) {
-    res.status(500).send("Internal server error. Please try again later.");
     return next(error);
   }
 });
