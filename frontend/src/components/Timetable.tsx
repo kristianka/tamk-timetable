ae1416a33e8f5fa73c35c18101e9c99279ad673c
Kristian <49764796+kristianka@users.noreply.github.com>
2024-03-25 14:06:11 +0200

    add search

--- frontend/src/components/Timetable.tsx
+++ frontend/src/components/Timetable.tsx
@@ -8,9 +8,9 @@ import { Reservation } from "../types";
 
 const Timetable = () => {
   const [timetable, setTimetable] = useState<Reservation[] | null>(null);
-  const [courseCode, setCourseCode] = useState<string>("5G00EV17-3003");
+  const [courseCode, setCourseCode] = useState<string>("");
   const [timetable2, setTimetable2] = useState<Reservation[] | null>(null);
-  const [classCode, setClassCode] = useState<string>("21i224");
+  const [classCode, setClassCode] = useState<string>("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,71 +19,97 @@ const Timetable = () => {
       const res2 = await getTimetableByClass(courseCode);
       setTimetable2(res2?.reservations || null);
     };
-    fetchData();
+    // fetchData();
   }, []);
+
   return (
     <div>
-      <p className="text-xl">Timetable</p>
-      <h1>Haku kurssilla, esim. 5G00EV17-3003</h1>
-      <Formik
-        initialValues={{ email: "" }}
-        onSubmit={async (values) => {
-          await new Promise((r) => setTimeout(r, 500));
-          alert(JSON.stringify(values, null, 2));
-        }}
-      >
-        <Form>
-          <label htmlFor="email">Email</label>
-          <Field
-            id="email"
-            name="email"
-            placeholder="jane@acme.com"
-            type="email"
-          />
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
+      <p className="m-5 text-xl">Timetable</p>
+      <div className="m-5">
+        <h2>Haku kurssilla, esim. 5G00EV17-3003</h2>
+        <Formik
+          initialValues={{ courseCode }}
+          onSubmit={async (values) => {
+            const res = await getTimetableByCourse(values.courseCode);
+            setTimetable(res?.reservations || null);
+          }}
+        >
+          <Form>
+            <Field
+              id="courseCode"
+              name="courseCode"
+              placeholder="5G00EV17-3003"
+              type="text"
+            />
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+      </div>
 
-      <table className="w-full text-sm text-left">
-        <thead className="text-xs">
-          <tr>
-            <th>Course</th>
-            <th>Starts</th>
-            <th>Ends</th>
-          </tr>
-        </thead>
-        <tbody>
-          {timetable &&
-            timetable.map((item: Reservation) => (
-              <tr className="bg-white border-b" key={item.id}>
-                <td>{item.subject}</td>
-                <td>{item.startDate}</td>
-                <td>{item.endDate}</td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+      <div className="m-5 divide-y">
+        <table className="w-full text-sm text-left">
+          <thead className="text-xs">
+            <tr>
+              <th>Course</th>
+              <th>Starts</th>
+              <th>Ends</th>
+            </tr>
+          </thead>
+          <tbody>
+            {timetable &&
+              timetable.map((item: Reservation) => (
+                <tr className="bg-white border-b" key={item.id}>
+                  <td>{item.subject}</td>
+                  <td>{item.startDate}</td>
+                  <td>{item.endDate}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
 
-      <h1>Haku ryhmäkoodilla 21i224</h1>
-      <table className="w-full text-sm text-left">
-        <thead className="text-xs">
-          <tr>
-            <th>Course</th>
-            <th>Starts</th>
-            <th>Ends</th>
-          </tr>
-        </thead>
-        <tbody>
-          {timetable2 &&
-            timetable2.map((item: Reservation) => (
-              <tr className="bg-white border-b" key={item.id}>
-                <td>{item.subject}</td>
-                <td>{item.startDate}</td>
-                <td>{item.endDate}</td>
-              </tr>
-            ))}
-        </tbody>
-      </table>
+      <div className="mt-10 m-5">
+        <h2>Haku ryhmäkoodilla, esim. 21i224</h2>
+
+        <Formik
+          initialValues={{ classCode }}
+          onSubmit={async (values) => {
+            const res = await getTimetableByCourse(values.classCode);
+            setTimetable2(res?.reservations || null);
+          }}
+        >
+          <Form>
+            <Field
+              id="classCode"
+              name="classCode"
+              placeholder="21i224"
+              type="text"
+            />
+            <button type="submit">Submit</button>
+          </Form>
+        </Formik>
+      </div>
+      <div className="m-5 divide-y">
+        <table className="w-full text-sm text-left">
+          <thead className="text-xs">
+            <tr>
+              <th>Course</th>
+              <th>Starts</th>
+              <th>Ends</th>
+            </tr>
+          </thead>
+          <tbody>
+            {timetable2 &&
+              timetable2.map((item: Reservation) => (
+                <tr className="bg-white border-b" key={item.id}>
+                  <td>{item.subject}</td>
+                  <td>{item.startDate}</td>
+                  <td>{item.endDate}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
