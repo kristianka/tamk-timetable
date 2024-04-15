c821aed2f6cdb6a3b1533c9a3dacd821e1a27001
Kristian <49764796+kristianka@users.noreply.github.com>
2024-04-15 11:36:51 +0300

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+// tailwind
+import "./App.css";
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
