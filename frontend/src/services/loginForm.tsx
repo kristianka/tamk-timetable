03e499240da369c909eebc16c9465fb1e83d6a2e
Niko Heikkila <nikoheikkilat@gmail.com>
2024-04-20 20:02:36 +0300

    Added token check on login

--- frontend/src/services/loginForm.tsx
+++ frontend/src/services/loginForm.tsx
@@ -1,6 +1,12 @@
 import axios from "axios";
 
 const login = async (username: string, password: string) => {
+  // check if token already stored in local storage
+  if (localStorage.getItem("token")) {
+    console.log("User is already logged in");
+    return;
+  }
+
   try {
     // post request to login endpoint
     const response = await axios.post("http://localhost:3000/api/login", {
