3f3b3e9bd58117b8aca7755ddde25ed1adc1e074
Niko Heikkilä <105411101+Zennrr@users.noreply.github.com>
2024-04-22 13:40:20 +0300

    Update login-spec.cy.tsx

--- frontend/cypress/e2e/login-spec.cy.tsx
+++ frontend/cypress/e2e/login-spec.cy.tsx
@@ -1,14 +1,11 @@
-// cypress/integration/login_spec.ts
-
 describe("Login", () => {
   it("successfully loads the login page", () => {
-    cy.visit("http://localhost:5173/login"); // change to your login page URL
+    cy.visit("http://localhost:5173/login");
   });
 
   it("can log in with correct credentials", () => {
     cy.visit("http://localhost:5173/login");
 
-    // Replace these with the correct selectors for your login form
     cy.get("input[name=username]").type("test");
     cy.get("input[name=password]").type("testpassword");
     cy.contains('button', 'Log in').click();
