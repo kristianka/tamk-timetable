a6c9b8410288abea67995578f16e86a289098dd9
Niko Heikkilä <105411101+Zennrr@users.noreply.github.com>
2024-04-25 13:26:33 +0300

    Remove redundant code

--- frontend/cypress/e2e/timetable-spec.cy.tsx
+++ frontend/cypress/e2e/timetable-spec.cy.tsx
@@ -11,15 +11,6 @@ describe("Timetable Addition and removal", () => {
     // Click the "Log in" button
     cy.contains("button", "Log in").click();
 
-    // Wait for the login operation to complete and store the token
-    cy.window().then((win) => {
-      const user = win.localStorage.getItem("user");
-      if (user) {
-        const obj = JSON.parse(user);
-        win["token"] = obj.token;
-      }
-    });
-
     // Type the course code
     cy.get("input[name=courseCode]").type("5G00EV17-3003");
 
